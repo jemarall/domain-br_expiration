d24a587e4bba1e08bf19b115d5a5e2155044d1ad
Jean <jean@macbook-air.local>
2022-08-24 02:36:34 -0300

    Lint teste 4

--- pages/registrobr.js
+++ pages/registrobr.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer-core');
     
-export default function registrobr(req, res) {
+export default (req, res) = async () => {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
